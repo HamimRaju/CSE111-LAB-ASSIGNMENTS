688493eb1c65939e38702a0273f159748626db4c
Hamim Islam Raju <119632722+HamimRaju@users.noreply.github.com>
2025-09-05 01:46:13 +0600

    Add files via upload

---
+++ Assignment-02/task_04/ImaginaryNumber.java
@@ -0,0 +1,10 @@
+package task_04;
+
+public class ImaginaryNumber {
+    int realPart = 0;
+    int imaginaryPart =0;
+
+    public void printNumber(){
+        System.out.println(realPart+" + "+imaginaryPart+"i");
+    }
+}

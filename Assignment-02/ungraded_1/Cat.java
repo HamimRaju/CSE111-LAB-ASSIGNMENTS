2abc3e3b199ca1994ecbc48f79b265e01705fccb
Hamim Islam Raju <119632722+HamimRaju@users.noreply.github.com>
2025-09-07 00:22:41 +0600

    Add files via upload

---
+++ Assignment-02/ungraded_1/Cat.java
@@ -0,0 +1,9 @@
+package ungraded_1;
+public class Cat {
+    String color = "White";
+    String action = "sitting";
+
+    public void printCat(){
+        System.out.println(color+" cat is "+action);
+    }
+}

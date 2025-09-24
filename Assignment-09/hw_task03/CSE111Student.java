1910b8bdba63f00fd12977bc59993efcb459849d
Hamim Islam Raju <119632722+HamimRaju@users.noreply.github.com>
2025-09-25 00:23:28 +0600

    Add files via upload

---
+++ Assignment-09/hw_task03/CSE111Student.java
@@ -0,0 +1,9 @@
+package hw_task03;
+
+public class CSE111Student extends Student{
+    public String msg = "I love Java programming";
+
+    public String shout(){
+        return msg;
+    }
+}
---
+++ Assignment-09/hw_task03/CSEStudent.java
@@ -0,0 +1,10 @@
+package hw_task03;
+
+public class CSEStudent extends Student{
+
+    public String msg = "I want to transfer to CSE";
+
+    public String shout(){
+        return msg;
+    }
+}
